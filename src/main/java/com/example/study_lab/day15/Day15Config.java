7396f5f5e0d58c8232c45331406bee2d4e91c846
홍주환 <ghdwnghks209@naver.com>
2026-01-14 02:14:54 +0900

    Security

--- src/main/java/com/example/study_lab/day15/Day15Config.java
+++ src/main/java/com/example/study_lab/day15/Day15Config.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -22,11 +23,18 @@ public class Day15Config {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+                //1. CSRF (Cross-Site Request Forgery) : GET로만 테스트하면 괜찮지만, POST시 403 포비든 -> 실습에서는 끈다.
+                .csrf(AbstractHttpConfigurer::disable)
+                //2. H2 콘솔을 위한 헤더 설정 : 시큐리티 기본적으로 <iframe> 막음(클릭재킹 방지) -> 그래서 H2로그인 불가(풀어줘야함)
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()))
+
+                //3. 권한 설정.
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/day15/public").permitAll()
                         .requestMatchers("/day15/private").authenticated()
                         .anyRequest().permitAll()
                 )
+                // 4. 로그인 폼.
                 .formLogin(Customizer.withDefaults());
 
         return http.build();
